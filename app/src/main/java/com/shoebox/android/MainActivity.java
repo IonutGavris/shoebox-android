7aa11d5f4c1b7d7454dbd52a5b9052f229af5f17
dianaghele <diana.ghele@3pillarglobal.com>
2015-11-25 15:53:17 +0200

    Change colors + updated main screen UI with links to second screens + ro/en strings + added basic activity for locations

--- app/src/main/java/com/shoebox/android/MainActivity.java
+++ app/src/main/java/com/shoebox/android/MainActivity.java
@@ -1,19 +1,15 @@
 package com.shoebox.android;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity
 		implements NavigationView.OnNavigationItemSelectedListener {
@@ -75,7 +71,7 @@ public class MainActivity extends BaseActivity
 		int id = item.getItemId();
 
 		if (id == R.id.nav_camera) {
-			startActivity(GenderAgePickerActivity.getLaunchingIntent(this));
+
 		} else if (id == R.id.nav_gallery) {
 
 		} else if (id == R.id.nav_slideshow) {
@@ -90,5 +86,15 @@ public class MainActivity extends BaseActivity
 
 		drawer.closeDrawer(GravityCompat.START);
 		return true;
+	}
+
+	@OnClick(R.id.boxContentBtn)
+	public void doBoxContent() {
+		startActivity(GenderAgePickerActivity.getLaunchingIntent(this));
+	}
+
+	@OnClick(R.id.dropLocationsBtn)
+	public void doDropLocations() {
+		startActivity(LocationsActivity.getLaunchingIntent(this));
 	}
 }
