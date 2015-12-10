767e7058d61ff6b99966d83a66b29ed531a52cbc
Ionut Gavris <gugionut@gmail.com>
2015-12-10 23:57:07 +0200

    Corrected strings Updated left drawer Removed unused libraries
    Former-commit-id: 712f01c8a0482614f0c4ee776d3f3a3c504fbecc

--- app/src/main/java/com/shoebox/android/MainActivity.java
+++ app/src/main/java/com/shoebox/android/MainActivity.java
@@ -1,11 +1,12 @@
 package com.shoebox.android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.shoebox.android.util.HelperClass;
@@ -51,33 +52,23 @@ public class MainActivity extends BaseActivity
 		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
 	@SuppressWarnings("StatementWithEmptyBody")
 	@Override
 	public boolean onNavigationItemSelected(MenuItem item) {
 		// Handle navigation view item clicks here.
 		drawer.closeDrawer(GravityCompat.START);
+
+		switch (item.getItemId()) {
+			case R.id.nav_contact:
+				Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "contact@shoebox.ro", null));
+				intent.putExtra(Intent.EXTRA_EMAIL, "contact@shoebox.ro");
+				intent.putExtra(Intent.EXTRA_SUBJECT, "ShoeBox Android");
+				startActivity(Intent.createChooser(intent, getString(R.string.send_email)));
+				break;
+			case R.id.nav_about:
+				startActivity(GettingStartedActivity.getLaunchingIntent(this));
+				break;
+		}
 		return true;
 	}
 
