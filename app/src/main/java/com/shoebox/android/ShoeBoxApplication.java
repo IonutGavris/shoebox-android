1e0ac9395f4ee64c660de52bd5ff88d017a648a0
Diana Sabau <diana.ghele@3pillarglobal.com>
2016-11-30 20:02:21 +0200

    Firebase: enable persistence for the data fetched from Firebase + disable analytics if debug build type

--- app/src/main/java/com/shoebox/android/ShoeBoxApplication.java
+++ app/src/main/java/com/shoebox/android/ShoeBoxApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.res.Resources;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.FirebaseDatabase;
 import com.shoebox.android.util.HelperClass;
 import com.shoebox.android.util.UIUtils;
 
@@ -20,22 +22,19 @@ public class ShoeBoxApplication extends Application {
 		super.onCreate();
 		phoneLocale = getResources().getConfiguration().locale;
 
-		// add JakeWharton's Timber logging library if debug build type
-		if (BuildConfig.DEBUG) {
-			Timber.plant(new Timber.DebugTree());
-		}
-
 		// Configure Fabric Kits
 		Fabric.with(this, new Crashlytics());
 
-		// configure Firebase
-//		Firebase.setAndroidContext(this);
-//
-//		Config config = new Config();
-//		config.setLogLevel(Logger.Level.INFO);
-//		config.setPersistenceEnabled(true);
-//
-//		Firebase.setDefaultConfig(config);
+		// enable persistence for the data fetched from Firebase
+		FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+		if (BuildConfig.DEBUG) {
+			// add JakeWharton's Timber logging library if debug build type
+			Timber.plant(new Timber.DebugTree());
+
+			// disable analytics if debug build type
+			FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(false);
+		}
 
 		if (UIUtils.LANG_RO.equals(phoneLocale.getLanguage())) {
 			HelperClass.addStringValueInSharedPreference(getApplicationContext(), HelperClass.keyAppLanguage, UIUtils.LANG_RO);
